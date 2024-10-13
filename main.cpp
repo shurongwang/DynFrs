02bf9b63cd2f2e0ea9507af25bd8ee610ad293c6
Shurong Wang <shurong.22@intl.zju.edu.cn>
2024-10-12 22:56:07 -0500

    Updated main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,63 @@
 
 // #include <bits/stdc++.h>
 #include <vector>
-#include "dynfrs.h"
+#include "DynFrs.h"
 #include "Disc/roc_auc.h"
 
 using namespace std;
 using namespace std::chrono;
 vector<vector<Tx>> X_train, X_test;
 vector<Ty> Y_train, Y_test;
+string dataset;
+auto t_start = high_resolution_clock::now(), t_end = t_start;
+
+unordered_map<string, int> T_default = {
+	{"Purchase",	250},
+	{"Vaccine",		250},
+	{"Adult",		100},
+	{"Bank",		250},
+	{"Heart",		150},
+	{"Diabetes",	250},
+	{"NoShow",		250},
+	{"Synthetic",	150},
+	{"Higgs",		100},
+};
+
+unordered_map<string, int> k_default = {
+	{"Purchase",	10},
+	{"Vaccine",		10},
+	{"Adult",		10},
+	{"Bank",		10},
+	{"Heart",		10},
+	{"Diabetes",	10},
+	{"NoShow",		10},
+	{"Synthetic",	10},
+	{"Higgs",		10},
+};
+
+unordered_map<string, int> d_default = {
+	{"Purchase",	10},
+	{"Vaccine",		20},
+	{"Adult",		20},
+	{"Bank",		20},
+	{"Heart",		15},
+	{"Diabetes",	30},
+	{"NoShow",		20},
+	{"Synthetic",	40},
+	{"Higgs",		30},
+};
+
+unordered_map<string, int> s_default = {
+	{"Purchase",	30},
+	{"Vaccine",		5},
+	{"Adult",		30},
+	{"Bank",		40},
+	{"Heart",		5},
+	{"Diabetes",	5},
+	{"NoShow",		5},
+	{"Synthetic",	30},
+	{"Higgs",		20},
+};
 
 void load_file(const string &filename, vector<vector<Tx>> &X, vector<Ty> &Y) {
 	FILE *buf_;
@@ -29,8 +79,7 @@ void load_file(const string &filename, vector<vector<Tx>> &X, vector<Ty> &Y) {
 	}
 }
 
-bool auc = 0;
-double eval(random_forest &RF, vector<vector<Tx>> X_test, vector<Ty> Y_test, bool dbg = 0) {
+double eval(random_forest &RF, const vector<vector<Tx>> &X_test, const vector<Ty> &Y_test, bool auc = 0) {
 	vector<double> res, pred(X_test.size());
 	int correct = 0;
 	for (int i = 0; i < X_test.size(); ++i) {
@@ -46,7 +95,51 @@ double eval(random_forest &RF, vector<vector<Tx>> X_test, vector<Ty> Y_test, boo
 	return result;
 }
 
-const bool dbg = 1;
+int unlearn_time_fixed(random_forest &RF, long long time_us) {
+	vector<int> id(X_train.size());
+	for (int i = 0; i < id.size(); ++i) id[i] = i;
+	shuffle(id.begin(), id.end(), mt);
+	
+	int cnt = 0;
+	long long elapsed = 0;
+	for (; elapsed < time_us && cnt < X_train.size(); ++cnt) {
+		t_start = high_resolution_clock::now();
+		RF.del(id[cnt]);
+		t_end = high_resolution_clock::now();
+		elapsed += duration_cast<microseconds>(t_end - t_start).count();
+	}
+	
+	return cnt;
+}
+
+int unlearn_count_fixed(random_forest &RF, int count) {
+	vector<int> id(X_train.size());
+	for (int i = 0; i < id.size(); ++i) id[i] = i;
+	shuffle(id.begin(), id.end(), mt);
+	
+	int cnt = 0;
+	long long elapsed = 0;
+	
+	if (!dly) {
+		for (; cnt < count && cnt < X_train.size(); ++cnt) {
+			t_start = high_resolution_clock::now();
+			RF.del(id[cnt]);
+			t_end = high_resolution_clock::now();
+			elapsed += duration_cast<microseconds>(t_end - t_start).count();
+		}
+	} else {
+		vector<int> to_del;
+		for (; cnt < count && cnt < X_train.size(); ++cnt) {
+			to_del.push_back(id[cnt]);
+		}
+		t_start = high_resolution_clock::now();
+		RF.del(to_del);
+		t_end = high_resolution_clock::now();
+		elapsed = duration_cast<microseconds>(t_end - t_start).count();
+	}
+	
+	return elapsed;
+}
 
 void upd(long long &sum, long long &min, long long &max, int t) {
 	sum += t;
@@ -54,48 +147,142 @@ void upd(long long &sum, long long &min, long long &max, int t) {
 	if (max == -1 || max < t) max = t;
 }
 
+void online_mixed_data_stream(random_forest &RF, int n_add, int n_del, int n_qry, bool print = 0) {
+	vector<double> res;
+	int size = Y_train.size();
+	long long cur;
+	long long add_sum = 0, add_min = -1, add_max = -1;
+	long long del_sum = 0, del_min = -1, del_max = -1;
+	long long qry_sum = 0, qry_min = -1, qry_max = -1;
+	unordered_map<int, bool> del;
+	
+	int n_req = n_add + n_del + n_qry;
+	vector<pair<int, int>> ops;
+	ops.reserve(n_req);
+	for (int i = 0; i < n_add; ++i) ops.push_back({0, n_qry + i});
+	for (int i = 0; i < n_del; ++i) ops.push_back({1, i});
+	for (int i = 0; i < n_qry; ++i) ops.push_back({2, i});
+	shuffle(ops.begin(), ops.end(), mt);
+	
+	int n_tot = n_add + n_del + n_qry, n_correct = 0, n_test = Y_test.size();
+	for (int i = 0; i < n_req; ++i) {
+		int op = ops[i].first, id = ops[i].second;
+		const auto &X = X_test[i];
+		const auto &Y = Y_test[i];
+		if (op == 0) {
+			t_start = high_resolution_clock::now();
+			RF.add(X, Y);
+			t_end = high_resolution_clock::now();
+			
+			cur = duration_cast<microseconds>(t_end - t_start).count();
+			upd(add_sum, add_min, add_max, cur);
+			
+			size += 1;
+			if (print) cerr << i << " add: " << cur << endl;
+		} else if (op == 1) {
+			int id = randint(0, size - 1);
+			for (; del.count(id); id = randint(0, size - 1));
+			
+			t_start = high_resolution_clock::now();
+			RF.del(id);
+			t_end = high_resolution_clock::now();
+			
+			cur = duration_cast<microseconds>(t_end - t_start).count();
+			upd(del_sum, del_min, del_max, cur);
+			del[id] = 1;
+			if (print) cerr << i << " del: " << cur << endl;
+		} else if (op == 2) {
+			t_start = high_resolution_clock::now();
+			RF.qry(X, res);
+			t_end = high_resolution_clock::now();
+			
+			cur = duration_cast<microseconds>(t_end - t_start).count();
+			upd(qry_sum, qry_min, qry_max, cur);
+			
+			n_correct += (res[1] >= 0.5) == Y;
+			if (print) cerr << i << " qry: " << cur << endl;
+			
+		}
+		RF.clean_up();
+	}
+	
+	cout << "add request:\t" << "mean " << (float) add_sum / n_add << "\tmin " << add_min << "\tmax " << add_max << endl;
+	cout << "del request:\t" << "mean " << (float) del_sum / n_del << "\tmin " << del_min << "\tmax " << del_max << endl;
+	cout << "qry request:\t" << "mean " << (float) qry_sum / n_qry << "\tmin " << qry_min << "\tmax " << qry_max << endl;
+	cout << (float) n_correct / n_qry << endl;
+}
+
+const bool dbg = 1;
+
+int T = 100, k = 10, max_dep = 20, n_unlearn = 0, mode = 1;
+long long time_us = 0;
+string data_dir = "./";
+vector<string> tasks;
+
+void parse(int argc, char *args[]) {
+	vector<string> flags;
+	for (int i = 1; i < argc; ++i) {
+		char *c = args[i];
+		string s = "";
+		for (; *c != '\0'; s += *c, ++c);
+		flags.push_back(s);
+	}
+	
+	for (int i = 0; i < flags.size();) {
+		const string &tp = flags[i++];
+		
+		if (tp == "-data") {
+			dataset = flags[i++];
+			data_dir = "Datasets/" + dataset + '/';
+		}
+		
+		else if (tp == "-auto") {
+			T = T_default[dataset];
+			k = k_default[dataset];
+			max_dep = d_default[dataset];
+			p_tries = s_default[dataset];
+		} else if (tp == "-T") {
+			T = atoi(flags[i++].c_str());
+		} else if (tp == "-k") {
+			k = atoi(flags[i++].c_str());
+		} else if (tp == "-d" || tp == "-max_dep") {
+			max_dep = atoi(flags[i++].c_str());
+		} else if (tp == "-s") {
+			p_tries = atoi(flags[i++].c_str());
+		}
+		
+		else if (tp == "-dly" || tp == "-delay") {
+			dly = atoi(flags[i++].c_str());
+		} else if (tp == "-mode") {
+			mode = atoi(flags[i++].c_str());
+		}
+		
+		else if (tp == "-acc") {
+			tasks.push_back(tp);
+		} else if (tp == "-auc") {
+			tasks.push_back(tp);
+		} else if (tp == "-unl_time") {
+			tasks.push_back(tp);
+			tasks.push_back(flags[i++]);
+		} else if (tp == "-unl_cnt") {
+			tasks.push_back(tp);
+			tasks.push_back(flags[i++]);
+		} else if (tp == "-stream") {
+			tasks.push_back(tp);
+			tasks.push_back(flags[i++]);
+			tasks.push_back(flags[i++]);
+			tasks.push_back(flags[i++]);
+		}
+	}
+	cerr << data_dir << ' ' << T << ' ' << k << ' ' << max_dep << ' ' << p_tries << ' ' << dly << ' ' << mode << endl;
+}
+
 int main(int argc, char *args[]) {
 	srand(time(NULL));
 	ios::sync_with_stdio(0);
 	
-	int T = 100, k = 10, max_dep = 20, n_unlearn = 0, mode = 1;
-	long long time_us = 0;
-	string data_dir = "./";
-
-	unordered_map<string, string> cmd;
-	for (int i = 1; i < argc; i += 2) {
-		string s1 = "", s2 = "";
-		char *c1 = args[i], *c2 = args[i + 1];
-		for (; *c1 != '\0'; s1 += *c1, ++c1);
-		for (; *c2 != '\0'; s2 += *c2, ++c2);
-		cmd[s1] = s2;
-	}
-	for (auto s : cmd) {
-		const string &tp = s.first, &r = s.second;
-		if (tp == "-data") {
-			data_dir = "Datasets/" + r + '/';
-		} else if (tp == "-T") {
-			T = atoi(r.c_str());
-		} else if (tp == "-k") {
-			k = atoi(r.c_str());
-		} else if (tp == "-d" || tp == "-max_dep") {
-			max_dep = atoi(r.c_str());
-		} else if (tp == "-s") {
-			p_tries = atoi(r.c_str());
-		} else if (tp == "-auc") {
-			auc = atoi(r.c_str());
-		} else if (tp == "-t" || tp == "-time") {
-			time_us = atoi(r.c_str()) * 1000;
-		} else if (tp == "-n_unl" || tp == "-n_unlearn") {
-			n_unlearn = atoi(r.c_str());
-		} else if (tp == "-dly" || tp == "-delay") {
-			dly = atoi(r.c_str());
-		} else if (tp == "-mode") {
-			mode = atoi(r.c_str());
-		}
-	}
-	cout << data_dir << ' ' << T << ' ' << k << ' ' << max_dep << ' ' << p_tries << ' ' << dly << ' ' << mode << endl;
-
+	parse(argc, args);
+	
 	load_file(data_dir + "train.txt", X_train, Y_train);
 	load_file(data_dir + "test.txt", X_test, Y_test);
 	
@@ -106,229 +293,35 @@ int main(int argc, char *args[]) {
 		cerr << "test size:  " << X_test.size() << ' ' << X_test[0].size() << endl;
 	}
 	
-	auto time_s = high_resolution_clock::now(), time_e = time_s;
-
-	time_s = high_resolution_clock::now();
-	
+	t_start = high_resolution_clock::now();
 	random_forest RF(X_train, Y_train, T, k, max_dep);
+	t_end = high_resolution_clock::now();
 	
-	time_e = high_resolution_clock::now();
-	auto duration = duration_cast<milliseconds>(time_e - time_s);
-	cout << "bui: " << duration.count() << "ms" << endl;	
-
-//	cout << eval(RF, X_test, Y_test) << endl;
-
-// test online data stream
-/*
-	int n_test = Y_test.size();
-	int n_correct = 0;
-	vector<double> res;
-	int cur, size = Y_train.size();
-	long long n_add = 0, add_sum = 0, add_min = -1, add_max = -1;
-	long long n_del = 0, del_sum = 0, del_min = -1, del_max = -1;
-	long long n_qry = 0, qry_sum = 0, qry_min = -1, qry_max = -1;
-	unordered_map<int, bool> del;
-	auto start = high_resolution_clock::now(), end = start;
+	auto duration = duration_cast<milliseconds>(t_end - t_start);
+	cout << "Build time:\t" << duration.count() << "ms" << endl;	
 	
-	// cerr << eval(RF, X_test, Y_test) << endl;
-	
-	// for (int i = 1001; i < X_train.size(); ++i) {
-	// 	RF.add(X_train[i], Y_train[i]);
-	// }
-	
-	// cerr << eval(RF, X_test, Y_test) << endl;
-	// return 0;
-
-	vector<vector<Tx>> X_test_2;
-	vector<Ty> Y_test_2;
-	bool print = 0;
-	for (int i = 0; i < 10000; ++i) {
-		const auto &X = X_test[i];
-		const auto &Y = Y_test[i];
-		const int B = 2;
-		if (i % B == 0) {
-			if ((i / B) >> 1 & 1) {
-				start = high_resolution_clock::now(); {
-					RF.add(X, Y);
-				} end = high_resolution_clock::now();
-				cur = duration_cast<microseconds>(end - start).count();
-				upd(add_sum, add_min, add_max, cur);
-				n_add += 1;
-				size += 1;
-				if (print) cerr << i << " add: " << cur << endl;
-			} else {
-				int id = randint(0, size - 1);
-				for (; del.count(id); id = randint(0, size - 1));
-				start = high_resolution_clock::now(); {
-					RF.del(id);
-				} end = high_resolution_clock::now();
-				cur = duration_cast<microseconds>(end - start).count();
-				upd(del_sum, del_min, del_max, cur);
-				n_del += 1;
-				del[id] = 1;
-				if (print) cerr << i << " del: " << cur << endl;
-			}
-		} else {
-			start = high_resolution_clock::now(); {
-				RF.qry(X, res);
-			} end = high_resolution_clock::now();
-			cur = duration_cast<microseconds>(end - start).count();
-			upd(qry_sum, qry_min, qry_max, cur);
-			
-			n_correct += (res[1] >= 0.5) == Y;
-			
-			n_qry += 1;
-			if (print) cerr << i << " qry: " << cur << endl;
-			
-			// X_test_2.push_back(X);
-			// Y_test_2.push_back(Y);
-		}
+	for (int i = 0; i < tasks.size();) {
+		const string &tp = tasks[i++];
 		
-		RF.clean_up();
-		
-		// cerr << '\t' << i << endl;
-	}
-	
-	cout << add_sum << ' ' << del_sum << ' ' << qry_sum << endl;
-	cout << "add: " << (float) add_sum / n_add << ' ' << add_min << ' ' << add_max << endl;
-	cout << "del: " << (float) del_sum / n_del << ' ' << del_min << ' ' << del_max << endl;
-	cout << "qry: " << (float) qry_sum / n_qry << ' ' << qry_min << ' ' << qry_max << endl;
-	cout << (float) n_correct / n_qry << endl;
-	
-	start = high_resolution_clock::now(); {
-		// RF.clean_up(1);
-	} end = high_resolution_clock::now();
-	cur = duration_cast<microseconds>(end - start).count();
-	cerr << "clean up: " << cur << endl;
-*/	
-	
-	// vector<vector<Tx>> X_train_2;
-	// vector<Ty> Y_train_2;
-	// for (int i = 0; i < RF.X.size(); ++i) {
-	// 	const auto &X = RF.X[i];
-	// 	const auto &Y = RF.Y[i];
-	// 	if (X.size() > 1) {
-	// 		X_train_2.push_back(X);
-	// 		Y_train_2.push_back(Y);
-	// 	}
-	// }
-	
-	// random_forest RF2(X_train_2, Y_train_2, T, k, max_dep);
-	// cout << eval(RF2, X_test_2, Y_test_2) << " but " << eval(RF, X_test_2, Y_test_2) << endl;
-	
-// test 1, 10, 100, .1%, 1.%
-/*
-	long long elapsed = 0, unlearn_count = 0;
-	vector<int> id(X_train.size());
-	for (int i = 0; i < id.size(); ++i) id[i] = i;
-	shuffle(id.begin(), id.end(), mt);
-
-	int n = X_train.size();
-
-//	vector<int> cnt = {1, 3, 10, 32, 100, 316, 1000, 3162, 10000, 31623, 100000, 316227, 1000000};
-	vector<int> cnt = {1, 10, 100, (int) (n * 0.001), (int) (n * 0.01)};
-	vector<double> time_used = vector<double>(cnt.size(), 0.0);
-	int cnt_max = *max_element(cnt.begin(), cnt.end());
-
-	for (int c = 1; c <= cnt_max; ++c) {
-		int idx = id[c - 1];
-		auto start = high_resolution_clock::now();
-		
-		RF.del(idx);
-
-		auto end = high_resolution_clock::now();
-		elapsed += duration_cast<microseconds>(end - start).count();
-
-		for (int i = 0; i < cnt.size(); ++i) {
-			if (c == cnt[i]) time_used[i] = elapsed / 1000.0;
+		if (tp == "-acc") {
+			double acc = eval(RF, X_test, Y_test, 0);
+			cout << "Accuracy:\t" << acc << endl;
+		} else if (tp == "-auc") {
+			double auc = eval(RF, X_test, Y_test, 1);
+			cout << "AUROC:\t\t" << auc << endl;
+		} else if (tp == "-unl_time") {
+			long long time_us = 1000LL * atoi(tasks[i++].c_str());
+			int cnt = unlearn_time_fixed(RF, time_us);
+			cout << "Unlearned:\t" << cnt << " samples within " << time_us / 1000 << " ms" << endl;
+		} else if (tp == "-unl_cnt") {
+			int cnt = atoi(tasks[i++].c_str());
+			long long time_us = unlearn_count_fixed(RF, cnt);
+			cout << "Unlearned:\t" << time_us / 1000 << " ms for " << cnt << " samples" << endl;
+		} else if (tp == "-stream") {
+			int n_add = atoi(tasks[i++].c_str()), n_del = atoi(tasks[i++].c_str()), n_qry = atoi(tasks[i++].c_str());
+			online_mixed_data_stream(RF, n_add, n_del, n_qry);
 		}
 	}
-
-	cout << "unl: ";
-	for (auto t : time_used) cout << t << ' ';
-	cout << endl;
-*/
-
-// other form of unlearning
-
-	long long elapsed = 0, unlearn_count = 0;
-	vector<int> id(X_train.size());
-	for (int i = 0; i < id.size(); ++i) id[i] = i;
-	shuffle(id.begin(), id.end(), mt);
 	
-	if (time_us) {
-		if (time_us < 0) {
-			time_s = high_resolution_clock::now();
-			
-			random_forest RF(X_train, Y_train, T, T, max_dep);
-			cerr << "build done" << endl;
-			
-			time_e = high_resolution_clock::now();
-
-			time_us = duration_cast<microseconds>(time_e - time_s).count();
-		}
-
-		counter = 0;
-		
-		for (int i = 0; elapsed < time_us && i < X_train.size(); ++i, unlearn_count += 1) {
-			auto start = high_resolution_clock::now();
-		
-			RF.del(id[i]);
-			
-			auto end = high_resolution_clock::now();
-			elapsed += duration_cast<microseconds>(end - start).count();
-		}
-		if (dly) RF.develop();
-		
-		cout << "unlearn count: " << unlearn_count << endl;
-	} else if (n_unlearn) {
-		if (mode == 1) {
-			cerr << "batch unlearning enabled" << endl;
-			vector<int> del(n_unlearn);
-			for (int i = 0; i < n_unlearn && i < X_train.size(); ++ i) {
-				del[i] = id[i];
-			}
-			auto start = high_resolution_clock::now();
-			
-			RF.del(del, dly);
-			
-			auto end = high_resolution_clock::now();
-			elapsed += duration_cast<microseconds>(end - start).count();
-		} else {
-			for (int i = 0; i < n_unlearn; ++i) {
-				auto start = high_resolution_clock::now();
-
-				RF.del(id[i]);
-
-				auto end = high_resolution_clock::now();
-				elapsed += duration_cast<microseconds>(end - start).count();
-			}
-		}
-
-		if (dly) {
-			auto start = high_resolution_clock::now();
-			RF.develop();
-			auto end = high_resolution_clock::now();
-			elapsed += duration_cast<microseconds>(end - start).count();
-		}
-		
-		cout << "unl " << n_unlearn << ": " << (double) elapsed / 1000 << " ms" << endl;
-	}
-
-//	cout << "unl_acc: " << eval(RF, X_test, Y_test, 0) << endl;
-
-	if (!time_us && !n_unlearn) return 0;
 	return 0;
-
-	vector<vector<Tx>> X_2;
-	vector<int> Y_2;
-	for (int i = n_unlearn; i < X_train.size(); ++i) {
-		int idx = id[i];
-		X_2.push_back(X_train[idx]);
-		Y_2.push_back(Y_train[idx]);
-	}
-
-	random_forest RF2(X_2, Y_2, T, k, max_dep);
-	cout << "nai_acc: " << eval(RF2, X_test, Y_test) << endl;
-
 }
